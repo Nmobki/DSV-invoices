19114bcf61ce011f33c2e396b472726195805b99
Nmobki <nmo@bki.dk>
2021-05-20 11:43:25 +0200

    Update DSV invoices.py

--- DSV invoices.py
+++ DSV invoices.py
@@ -25,12 +25,39 @@ Engine = create_engine('mssql+pyodbc:///?odbc_connect=%s' % Params)
 # Other variables
 Timestamp = datetime.datetime.now()
 Script_name = 'DSV invoices.py'
+I = 0
+Files_count = len(Path_Glob)
+Files_error = []
+
+Cols_int = ['Invoice date','Delivery_Date','Pickup_Date','Account_Code']
+Cols_dec = ['Colli','Grossweight','Billing_Weight','LDM','CBM','Exchange_Rate','VAT_Amount','Amount']
+Cols_string = ['Filename','Invoice no','Customer','Marks','Collitype','Content','Department','Traffic','Transport','Trip'
+               ,'Consignment_Number','Customer_Ref1','Customer_Ref2','Terms','Shipper_Name','Shipper_Address1'
+               ,'Shipper_Address2','Shipper_Address3','Shipper_Address4','Shipper_Country','Shipper_Postcode'
+               ,'Consignee_Name','Consignee_Address1','Consignee_Address2','Consignee_Address3','Consignee_Address4'
+               ,'Consignee_Country','Consignee_Postcode','Equipment1','Equipment2','Equipment_Type','Text','Currency'
+               ,'Calculation and texts','Sub Customer']
 
 
 for File_name in Files_in_path:
-    print(File_name) # Sti til fil inkl. selve filen
-    print(File_name.replace(Path_source ,'' ,1)) # Det reelle filnavn
-
-    Df_file = pd.read_csv(File_name ,header=0 ,sep=';').to_dict()
+    try:
+        Df_file = pd.read_csv(File_name ,header=0 ,sep=';' ,thousands=',')
+        Df_file.loc[: ,'Filename'] = File_name.replace(Path_source ,'' ,1)
+        
+        Df_file[Cols_string] = Df_file[Cols_string].astype(str)
+        Df_file[Cols_dec] = Df_file[Cols_dec].apply(pd.to_numeric)
+        
+        Df_file[Cols_int] = Df_file[Cols_int].fillna(99999999)
+        Df_file[Cols_int] = Df_file[Cols_int].astype(int)
     
-    print(Df_file)
+        Df_file = Df_file.replace({'nan':None ,'NONE':None ,'NaN':None ,99999999:None}) # Convert text NULL values to actual NULL values
+        
+        I += 1
+    except:
+        Files_error.append(File_name)
+        pass
+
+    Df_file.to_sql('DSV_fakturaspecifikationer' ,con=Engine ,schema=Schema ,if_exists='append' ,index=False) #Insert file into SQL
+
+print(I)
+print(Files_error)
